9e59403cc7acad34e92eb95b93a24cd1425410dc
Dineshreddy13 <bijjamdineshreddy@gmail.com>
2026-08-28 11:03:22 +0530

    add trust proxy

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ import logger from "./utils/logger.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
+app.set("trust proxy", 1); // Trust first proxy (Render load balancer)
 const httpServer = http.createServer(app);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
