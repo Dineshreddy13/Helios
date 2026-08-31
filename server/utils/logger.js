1fae7933313b897a7bf681bb17b09973102e80bc
Dineshreddy13 <bijjamdineshreddy@gmail.com>
2026-08-31 11:30:24 +0530

    feature: add http logs for the server

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -2,16 +2,18 @@ import pino from "pino";
 
 const logger = pino({
     level: process.env.LOG_LEVEL || "info",
-    transport: {
-        target: "pino-pretty",
-        options: {
-            colorize: true,
-            translateTime: "SYS:standard",
-            ignore: "pid,hostname",
-            messageFormat: "{msg}",
-            hideObject: true,
-        },
-    },
+    transport: process.env.NODE_ENV !== "production"
+        ? {
+            target: "pino-pretty",
+            options: {
+                colorize: true,
+                translateTime: "SYS:standard",
+                ignore: "pid,hostname",
+                messageFormat: "{msg}",
+                hideObject: true,
+            },
+        }
+        : undefined,
 });
 
 export default logger;
